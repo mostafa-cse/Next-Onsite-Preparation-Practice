d7f2325c47d499a172d0702ea7cbd2dce9cc9c03
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-06-16 14:29:14 +0600

    Accepted!

---
+++ Day - 17/J_Winter.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+void solve() {
+    
+}
+signed main() {
+    int tc;
+    cin >> tc;
+
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case " << t << ": ";
+        solve();
+    }
+    return 0;
+}
