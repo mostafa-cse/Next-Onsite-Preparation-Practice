95681cd049e05bacbca5a351374b71ee5c78d017
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-06-16 17:00:21 +0600

    Refactor solve function to improve readability and structure

--- Day - 17/J_Winter.cpp
+++ Day - 17/J_Winter.cpp
@@ -2,14 +2,32 @@
 #define int long long
 using namespace std;
 void solve() {
+    int n, k;
+    cin >> n >> k;
     
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin() + 1, a.end());
+
+    vector<int> dp(n + 1, 1e18);
+    dp[0] = 0;
+    for (int r = 3,l = 0; r <= n; ++r) {
+        while (a[r] - a[l + 1] > k * 2) l++;
+        while (l < r - 2 and dp[l] == 1e18) l++;
+        if (l < r - 2) {
+            dp[r] = min(dp[r], dp[l] + 1);
+        }
+    }
+    cout << (dp[n] == 1e18 ? -1 : dp[n]) << endl;
 }
 signed main() {
     int tc;
     cin >> tc;
 
     for (int t = 1; t <= tc; t++) {
-        // cout << "Case " << t << ": ";
+        cout << "Case " << t << ": ";
         solve();
     }
     return 0;
